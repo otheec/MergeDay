84c151ccecac83a5847f8c8f899e2e81d4cf5f74
Otmar Nezdařil <otmar.nezdaril@itixo.com>
2025-08-14 23:10:16 +0200

    removed unused components from template

--- src/MergeDay.Client/src/pages/Dashboard/Home.tsx
+++ src/MergeDay.Client/src/pages/Dashboard/Home.tsx
@@ -3,7 +3,6 @@ import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
 import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import RecentOrders from "../../components/ecommerce/RecentOrders";
-import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
 
 export default function Home() {
@@ -26,10 +25,6 @@ export default function Home() {
 
         <div className="col-span-12">
           <StatisticsChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <DemographicCard />
         </div>
 
         <div className="col-span-12 xl:col-span-7">
