3ff63ffd5620f98c3279dfec76bd5c668f500d3e
Otmar Nezdařil <nezdaril.o@seznam.cz>
2025-08-19 19:46:20 +0200

    fixed toggl url

--- src/MergeDay.Api/Features/Toggl/Connector/ITogglApi.cs
+++ src/MergeDay.Api/Features/Toggl/Connector/ITogglApi.cs
@@ -5,7 +5,7 @@ namespace MergeDay.Api.Features.Toggl.Connector;
 
 public interface ITogglApi
 {
-    [Get("/api/v9/me/time_entries")]
+    [Get("/me/time_entries")]
     Task<List<TogglTimeEntryDto>> GetTimeEntriesAsync(
         [AliasAs("start_date")] string startDateIso,
         [AliasAs("end_date")] string endDateIso,
