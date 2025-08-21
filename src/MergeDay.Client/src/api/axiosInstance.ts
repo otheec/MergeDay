1c8d1928a16beb8db3cef5e93aac720ebd679501
Radim Kotajny <radimkotajny@gmail.com>
2025-08-21 16:56:54 +0200

    added basic setup for axiosInstance.ts

---
+++ src/MergeDay.Client/src/api/axiosInstance.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const anonymousInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+})
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+})
